8b9d7a4c02b9fbfe74952c5dc680b8754f1f906c
wdantas <wadantasjr@gmail.com>
2021-04-08 23:39:29 -0300

    -Set Bone´s boxcolliders2d as triggers -Created Bonuses prefabs -Reorganizing the prefabs folders

---
+++ Mouse Trap Remake/Assets/Scripts/Bonuses.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonuses : MonoBehaviour
+{
+    public GameObject[] bonuses = null;
+    public int currentId = -1;
+}
